11b1220316936cfc2c0c7196a84d64d67e55fb2d
Michael Ramos <michaelaramos182@gmail.com>
2022-05-04 00:01:41 -0500

    Added in get requests to pull current weather and uvIndex from open weather api. Added in logic to assign uv color to correct index.

--- Assets/index.js
+++ Assets/index.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
     // create an array for cities to be held
     let cityArray = localStorage.getItem("cities");
@@ -13,7 +13,7 @@ $(document).ready(function () {
     // function to show cities from search bar
 
     function showCities() {
-        let searchResults = $("#search-results")
+        let searchResults = $("#searchResults")
         searchResults.empty();
     }
 
@@ -23,11 +23,63 @@ $(document).ready(function () {
         searchResults.prepend(city);
     }
 
+    // connect to weather api and call requests
+
+    function weatherCall(cityInput) {
+
+        let apiKey = 'f55bbae1950a45872318ea4d866e7b67';
+        let currentWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=" + apiKey + "&units=imperial";
+    
+    // ajax call to get current weather of city search
+    $.ajax({
+        url: apiKey,
+        method: "GET"
+    }).then(function (response) {
+
+        $("#city").text(response.name);
+        $("#temp").html("Temperature " + response.main.temp + " &#176; F");
+        $("#wind").text("Wind Speed: " + response.wind.speed + " mph");
+        $("#humidity").text("Humidity: " + response.main.humidity + "%");
+
+        let uvResponse = "https://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey + "&lat=" + response.coord.lat + "&lon=" + response.coord.lon 
+
+        // ajax call to get UV color from api
+        $.ajax({
+            url: uvResponse,
+            method: "GET"
+        }).then(function (uvIndex) {
+
+            $("#uvIndex").text("UV Index: " + uvIndex.value);
+
+        // conditional to change color of uv index
+        if (uvIndex.value <= 2) {
+            $("#uvIndex").addClass(".text-light");
+            $("#uvIndex").attr("style", "background-color: green");
+        } else if (uvIndex.value > 3 && uvIndex.value <= 5) {
+            $("#uvIndex").addClass(".text-light");
+            $("#uvIndex").attr("style", "background-color: yellow");
+        } else if (uvIndex.value > 5 && uvIndex.value <= 7) {
+            $("#uvIndex").addClass(".text-light");
+            $("#uvIndex").attr("style", "background-color: orange");
+        } else if (uvIndex.value > 7 && uvIndex.value < 10) {
+            $("#uvIndex").addClass(".text-light");
+            $("#uvIndex").attr("style", "background-color: red");
+        } else if (uvIndex.value > 11) {
+            $("#uvIndex").addClass(".text-light");
+            $("#uvIndex").attr("style", "background-color: violet");
+        }
+
+        })
+    
+    })
+
+}
+
     // click event to search cities
     $("#search-btn").click(function () {
         let cityInput = $("#search-input").val();
         cityArray.push(cityInput)
-        queries(cityInput)
+        weatherCall(cityInput)
 
     // placing city inputs into local storage
     localStorage.setItem("cities", JSON.stringify(cityArray));
